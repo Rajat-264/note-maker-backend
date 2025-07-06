96b8c180b222bfa447dae7a678345a0eace032fa
Rajat-264 <handerajat@gmail.com>
2025-07-06 22:11:01 +0530

    server.js changed

--- server.js
+++ server.js
@@ -5,16 +5,17 @@ import authRoutes from './routes/authRoutes.js';
 import topicRoutes from './routes/topicRoutes.js';
 import cors from 'cors';
 
-dotenv.config();
-connectDB();
-
 const app = express();
 app.use(cors({
   origin: ['https://note-maker-frontend.vercel.app'],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+
+dotenv.config();
+connectDB();
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
