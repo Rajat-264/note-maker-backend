796e1adb14032d9b5237e215104d4446b233bbb1
Rajat-264 <handerajat@gmail.com>
2025-07-06 23:07:37 +0530

    server.js changed

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+app.options('*', cors());
 
 dotenv.config();
 connectDB();
