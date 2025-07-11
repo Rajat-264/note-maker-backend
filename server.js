088bd55a1070f5a202dbfad036012147eddd89ae
Rajat-264 <handerajat@gmail.com>
2025-07-11 18:29:57 +0530

    ping added

--- server.js
+++ server.js
@@ -21,5 +21,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/topics', topicRoutes);
 
+app.get('/ping', (req, res) => res.send('pong'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
